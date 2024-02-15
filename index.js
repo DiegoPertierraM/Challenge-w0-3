19ec6bc76b5da503890e9e75a5ac067103ec5160
DiegoPertierraM <diegopmenendez@gmail.com>
2024-02-15 10:16:17 +0100

    Code correction

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import { strictEquals } from './module.js';
 
-console.log(strictEquals(1, 1)); // true
-console.log(strictEquals(NaN, NaN)); // false --> Rule
-console.log(strictEquals(0, -0)); // true --> Rule
-console.log(strictEquals(-0, 0)); // true --> Rule
-console.log(strictEquals(1, '1')); // false
-console.log(strictEquals(true, false)); // false
-console.log(strictEquals(false, false)); // true
-console.log(strictEquals('water', 'oil')); // false
+console.log('Testing 1 v. 1: expected true', strictEquals(1, 1));
+console.log(
+  'Testing NaN v. NaN: expected false',
+  'Rule',
+  strictEquals(NaN, NaN)
+);
+console.log('Testing 0 v. -0: expected true', 'Rule', strictEquals(0, -0));
+console.log('Testing -0 v. 0: expected true', 'Rule', strictEquals(-0, 0));
+console.log('Testing 1 v. "1": expected false', strictEquals(1, '1'));
+console.log('Testing true v. false: expected false', strictEquals(true, false));
+console.log(
+  'Testing false v. false: expected true',
+  strictEquals(false, false)
+);
+console.log(
+  'Testing "water" v. "oil": expected false',
+  strictEquals('water', 'oil')
+);
+
+// CORRECIONES
+// --------------------------------------------------------------------------------
+// Añadir en el testing lo que debe dar en vez de comentarlo
--- module.js
+++ module.js
@@ -1,17 +1,31 @@
-export { strictEquals };
+// CORRECIONES
+// --------------------------------------------------------------------------------
+// export { strictEquals }; --> Se haría al final el export
+// --> Exporta algo que todavía no existe, funciona por el hoisting de JS
+// --> Además, es más habitual exportar una a una las funciones
+// --> También se puede exportar por defecto: export default { strictEquals }
 
-const strictEquals = (a, b) => {
+export const strictEquals = (a, b) => {
   if (Number.isNaN(a) && Number.isNaN(b)) {
     return false;
   }
+
   if (
     (Object.is(a, -0) && Object.is(0, b)) ||
     (Object.is(a, 0) && Object.is(-0, b))
   ) {
     return true;
   }
-  if (Object.is(a, b)) {
-    return true;
-  }
-  return false;
+
+  return Object.is(a, b);
+
+  // CORRECIONES
+  // --------------------------------------------------------------------------------
+  // if (Object.is(a, b)) {
+  //   return true;
+  // }
+  // return false;
+
+  // --> Object.is(a,b) devuelve un boolean, por lo que se puede devolver directamente
+  // --> No tiene sentido hacer un condicional, sobra
 };
